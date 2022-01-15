c7a8400bca07057da8d75eb927e7f69feec2cfac
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-15 14:38:34 -0600

    code updated 1/15/2022 2:36pm

--- list printer current .py
+++ list printer current .py
@@ -3,23 +3,18 @@
 import statistics
 the_list = []
 
-# This function contains the code that fills the list.
-def list_printer():
+print('''--- welcome to list printer ---
+you will be allowed to fill the list, and then the program will make calculations for the list. Enjoy :D
+''')
+# This is the code that fills the list.
+# This try and except part will quit the program if the user enters something that causes an error in the program.
+try:
     times_asked = 0
     how_many_numbers = int(input('Enter the amount of numbers you want there to be in your list: '))
     for x in range(how_many_numbers):
         times_asked = 1 + times_asked
         enter_number_in_list = int(input(f'{times_asked}. Enter a number: '))
         the_list.append(enter_number_in_list)
-
-
-print('''--- welcome to list printer ---
-you will be allowed to fill the list, and then the program will make calculations for the list. Enjoy :D
-''')
-
-# This try and except part will quit the program if the user enters something that causes an error in the program.
-try:
-    list_printer()
 except:
     print("Only numbers please. Manually restart the code because I don't know how to make it restart itself. ")
     quit()
@@ -37,7 +32,7 @@ for number in the_list:
     print(f'{count}          {number}')
 
 
-
+# the calculations
 print('''---------------------------
 the calculations:
 ''')
@@ -52,19 +47,30 @@ print(f'biggest: {biggest}')
 print(f'smallest: {smallest}')
 print(f'median: {median}')
 
+# this part checks how many numbers are within a certain range(that the user inputs)
+print('''
+--- range checker ---''')
 
-
+amount_of_numbers_in_theRange = 0
+amount_of_numbers_not_in_theRange = 0
 check_range = input('''
-Do you want me to check if a certain number is in a list? If so, enter 'yes':  ''')
+Do you want me to check how many numbers from the list are in a certain range? If so, enter 'yes':  ''')
 try:
     if check_range == 'yes':
         print(f'Here is your list: {the_list}')
-        what_is_range = int(input('Enter the range: '))
+        what_is_range = int(input('Enter the range 0-'))
+        print(' ')
         for number in the_list:
-            if number in range(what_is_range):
+            if number in range(what_is_range + 1):
+                amount_of_numbers_in_theRange = amount_of_numbers_in_theRange + 1
                 print(f'Is within 0-{what_is_range}: {number}')
             else:
+                amount_of_numbers_not_in_theRange = amount_of_numbers_not_in_theRange + 1
                 print(f"Is not within 0-{what_is_range}: {number} ")
+        print(' ')
+        print(f'Amount of numbers within 0-{what_is_range}: {amount_of_numbers_in_theRange}')
+        print(f'Amount of numbers not within 0-{what_is_range}: {amount_of_numbers_not_in_theRange}')
+
     else:
         print('Ok. thank you for using list printer :).')
         quit()
