ff00c1752805e91381c94cd9a4bf7ae0542f187b
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-15 21:36:54 -0600

    code updated 1/15/2022 8:18pm

--- list printer current .py
+++ list printer current .py
@@ -74,4 +74,42 @@ try:
         print('Ok. thank you for using list printer :).')
         quit()
 except:
-    quit(print('pls dont breAK the code'))
+    quit()
+
+
+# this will print all the results onto a file called out.txt
+from contextlib import redirect_stdout
+
+with open('out.txt', 'w') as f:
+    with redirect_stdout(f):
+        print(f'The list: {the_list} ')
+        count = 0
+        print('---------------------------')
+        print(' count | number ')
+        # this adds 1 to count for each number in the list
+        for number in the_list:
+            count = 1 + count
+            print(f'{count}          {number}')
+        print('---------------------------')
+        print('the calculations:')
+        print(' ')
+        print(f'sum: {sum}')
+        print(f'average: {int(average)}')
+        print(f'biggest: {biggest}')
+        print(f'smallest: {smallest}')
+        print(f'median: {median}')
+
+        print(' ')
+        print('--- range checker ---')
+        print(f'Here is your list: {the_list}')
+        for number in the_list:
+            if number in range(what_is_range + 1):
+                amount_of_numbers_in_theRange = amount_of_numbers_in_theRange + 1
+                print(f'Is within 0-{what_is_range}: {number}')
+            else:
+                amount_of_numbers_not_in_theRange = amount_of_numbers_not_in_theRange + 1
+                print(f"Is not within 0-{what_is_range}: {number} ")
+        print(' ')
+        print(f'Amount of numbers within 0-{what_is_range}: {amount_of_numbers_in_theRange}')
+        print(f'Amount of numbers not within 0-{what_is_range}: {amount_of_numbers_not_in_theRange}')
+
